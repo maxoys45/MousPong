ca9d6d85d0f0dd23be968351f5e806d67139f983
Max Henchman <max.henchman@mous.co>
2020-01-29 20:52:20 +0000

    switching to live DB

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,11 @@
 const prod = {
+  // MongoURI: 'mongodb+srv://maxoys45:Mongoys2k20@mouspong-v85w0.mongodb.net/db_test?retryWrites=true&w=majority'
   MongoURI: 'mongodb+srv://maxoys45:Mongoys2k20@mouspong-v85w0.mongodb.net/db?retryWrites=true&w=majority'
 }
 
 const dev = {
   MongoURI: 'mongodb://localhost:27017/mouspong'
+  // MongoURI: 'mongodb://localhost:27017/mouspong2'
 }
 
-module.exports = dev
+module.exports = prod
