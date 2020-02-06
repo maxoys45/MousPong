4999759f435e8068561ef7064a32c73747f74c0f
Max Henchman <max.henchman@mous.co>
2020-02-06 07:52:36 +0000

    resetting db to live

--- config/keys.js
+++ config/keys.js
@@ -2,4 +2,4 @@ const prod = 'mongodb+srv://maxoys45:Mongoys2k20@mouspong-v85w0.mongodb.net/db?r
 const dev_atlas = 'mongodb+srv://maxoys45:Mongoys2k20@mouspong-v85w0.mongodb.net/db_dev?retryWrites=true&w=majority'
 const dev = 'mongodb://localhost:27017/mouspong'
 
-module.exports = process.env.MONGODB_URL || dev
+module.exports = process.env.MONGODB_URL || prod
