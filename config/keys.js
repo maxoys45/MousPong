94cf164d02e7a88bd9aa81630e5ed2525ea65692
Max Henchman <max.henchman@mous.co>
2020-02-18 11:38:28 +0000

    amending modal slightly

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,6 @@ const db = 'mongodb+srv://maxoys45:Mongoys2k20@mouspong-v85w0.mongodb.net/db?ret
 // const db = 'mongodb://localhost:27017/mouspong'
 // const db = 'mongodb://localhost:27017/mouspong_remove'
 // const db = 'mongodb://localhost:27017/mouspong_elo'
+// const db = 'mongodb://localhost:27017/mouspong_delete'
 
 module.exports = process.env.MONGODB_URL || db
